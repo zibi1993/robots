3df2c9be2304c842dd24565e73399b38f9e59aa5
zibi1993 <s.zygmunt1993@gmail.com>
2019-04-10 14:50:42 +0200

    add missing folders from another project

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
+import React from 'react'; 
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import {robots} from './robots';
+const App = ()=>{
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div className='tc'>
+        <h1>Robo Friends</h1>
+        <SearchBox/>
+        <CardList robots = {robots}/> 
+        </div>
+    )
 }
 
 export default App;
---
+++ robofriends/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Card =({name, email, id, username})=>{
+    return (
+    <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+    <img alt ='robots' src={`https://robohash.org/${id}?200x200`}/>
+    <div>
+    <h1>{name}</h1>
+    <h2>{email}</h2>
+    <h2>{username}</h2>
+    </div>
+    </div>
+    );
+}
+
+export default Card;  
