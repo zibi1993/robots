1378dbd06bbcf14a108dd9b0e1b032a139736d09
zibi1993 <s.zygmunt1993@gmail.com>
2019-04-10 15:29:37 +0200

    add syntax in App and create SearchChange

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,15 +1,30 @@
-import React from 'react'; 
+import React, {Component} from 'react'; 
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 import {robots} from './robots';
-const App = ()=>{
+
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchfiled: ''
+        }
+    }
+    onSearchChange(event){
+        console.log(event); 
+    }
+
+    render(){ 
     return (
         <div className='tc'>
         <h1>Robo Friends</h1>
-        <SearchBox/>
-        <CardList robots = {robots}/> 
+        <SearchBox searchChange = {this.onSearchChange}/>
+        <CardList robots = {this.state.robots}/> 
         </div>
     )
+}
 }
 
 export default App;
--- robofriends/src/SearchBox.js
+++ robofriends/src/SearchBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const SearchBox =()=>{
+const SearchBox =({searchfiled,searchChange})=>{
     return (
         <div className ='pa2'>
         <input 
         className = 'pa3 ba b--green bg-lightest-blue '
         type ='search' 
-        placeholder='search robots'/> 
+        placeholder='search robots'
+        onChange= {searchChange}
+        />
         </div>
+
     );
 
     
