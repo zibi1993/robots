26aaae6847c1270f701ffb91ab18930b82b2bc36
zibi1993 <s.zygmunt1993@gmail.com>
2019-04-12 12:01:51 +0200

    add Compoment Lifecycle, component DidMount, how its works

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -8,9 +8,20 @@ class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfiled: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>{
+            return response.json();
+        })
+        .then(user=>{
+            this.setState({robots: user});
+        })
+       
     }
     onSearchChange=(event)=>{
         this.setState({searchfiled: event.target.value})
@@ -20,13 +31,19 @@ class App extends Component{
         const filterRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfiled.toLowerCase());
         })
-    return (
-        <div className='tc'>
-        <h1 className='f1'>Robo Friends</h1>
-        <SearchBox searchChange = {this.onSearchChange}/>
-        <CardList robots = {filterRobots}/> 
-        </div>
-    )
+        if (this.state.robots.length === 0){
+            return <h1>Loading</h1>
+        }else {
+            return(
+            <div className='tc'>
+            <h1 className='f1'>Robo Friends</h1>
+            <SearchBox searchChange = {this.onSearchChange}/>
+            <CardList robots = {filterRobots}/> 
+            </div>
+            );
+        }
+
+    
 }
 }
 
